885e10a66637d78d5f2100ef1045c116baea2848
yagizbora <yaizbora@gmail.com>
2023-09-22 16:58:08 +0300

    Add project files.

---
+++ İş yeri kullanıcı girişleri/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql;
+using MySql.Data.MySqlClient;
+
+
+
+namespace İş_yeri_kullanıcı_girişleri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
