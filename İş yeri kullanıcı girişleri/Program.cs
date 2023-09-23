266330e401da20cc66a534c2c408ba86e7b1edbf
yagizbora <yaizbora@gmail.com>
2023-09-23 18:34:06 +0300

    Server bağlantısı için komutlar yazıldı bağlantı henüz tamamlanmadı#

--- İş yeri kullanıcı girişleri/Program.cs
+++ İş yeri kullanıcı girişleri/Program.cs
@@ -15,12 +15,25 @@ namespace İş_yeri_kullanıcı_girişleri
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+          static void Main(string[] args)
         {
+            string server = "127.0.0.1";
+            string database = "employeer_list";
+            string username = "root";
+            string password = "root";
+            string constring = "SERVER=" + server + "" + "DATABASE=" + database + ";" +
+                "UID=" + username + ";" + "PASSWORD=" + password + ";";
+                MySqlConnection conn = new MySqlConnection(constring);
+            conn.Open();
+            string query = "select * from employeer_list";
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Application.Run(new Form2());
         }
     }
 }
-
